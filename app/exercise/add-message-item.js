e96475e9ebc9e297b897a1fa1b19ff12f6f51a30
Evan Engels <evan.engels@gmail.com>
2017-02-17 09:49:27 -0600

    small change

--- app/exercise/add-message-item.js
+++ app/exercise/add-message-item.js
@@ -2,6 +2,5 @@ import createMessageItem from './create-message-item';
 
 export default function addMessageItem(list, message) {
   const li = createMessageItem(message);
-
   list.appendChild(li);
 };
